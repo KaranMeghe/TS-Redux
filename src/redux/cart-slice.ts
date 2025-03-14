f9673fe5ce91a76aaf63002378875c999aae029e
Karan Meghe <karanmeghe87@gmail.com>
2025-03-15 03:29:49 +0530

    feat: Creating Redux store & cartSlice by adding logic to reducers

---
+++ src/redux/cart-slice.ts
@@ -0,0 +1,49 @@
+/** @format */
+
+import { createSlice type PayloadAction } from '@reduxjs/toolkit';
+
+interface CARTITEM {
+  id: string;
+  title: string;
+  price: number;
+  quantity: number;
+}
+
+interface CARTSTATE {
+  items: CARTITEM[];
+}
+
+const initialState: CARTSTATE = {
+  items: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+
+    addToCart:(state, action: PayloadAction<{id:string;  title: string;  price: number;}>) => {
+      const itemIndex = state.items.findIndex((item) => item.id === action.payload.id);
+
+      if(itemIndex >= 0){
+        state.items[itemIndex].quantity++;
+      } else {
+        state.items.push({...action.payload, quantity: 1})
+      }
+    },
+
+    removeItem:(state, action: PayloadAction<string>) => {
+     const itemIndex = state.items.findIndex((item) => item.id === action.payload);
+
+     if(state.items[itemIndex].quantity === 1){
+        state.items.splice(itemIndex,1);
+     } else {
+        state.items[itemIndex].quantity--
+     }    
+    },
+
+
+  },
+});
+
+export const cartReducer = cartSlice.reducer;
